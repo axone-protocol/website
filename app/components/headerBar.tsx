35677b46ffed88dc07d4115ae606b311b6705add
adriendillens <adriendillens@gmail.com>
2024-02-28 18:09:59 +0100

    navigation/constants file for internal and external urls
    # headerBar urls refactored
    # home buttons urls refactored

--- app/components/headerBar.tsx
+++ app/components/headerBar.tsx
@@ -3,6 +3,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Image from "next/image";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { internalUrls, externalUrls } from "app/lib/navigation/constants";
 import AxoneLogo from "public/images/axone-logo.svg";
 
 export default function HeaderBar() {
@@ -15,7 +16,7 @@ export default function HeaderBar() {
       data-bs-theme="light"
     >
       <Container fluid>
-        <Navbar.Brand href="/">
+        <Navbar.Brand href={internalUrls.home}>
           <Image src={AxoneLogo} priority width="150" alt="Axone logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -23,48 +24,42 @@ export default function HeaderBar() {
           <Nav className="me-auto" />
           <Nav>
             <NavDropdown title="LEARN" className="fw-bold">
-              <NavDropdown.Item href="">About Axone</NavDropdown.Item>
-              <NavDropdown.Item href="/roadmap">Roadmap</NavDropdown.Item>
-              <NavDropdown.Item href="https://docs.okp4.network/academy/overview">
+              <NavDropdown.Item href={internalUrls.about}>
+                About Axone
+              </NavDropdown.Item>
+              <NavDropdown.Item href={internalUrls.roadmap}>
+                Roadmap
+              </NavDropdown.Item>
+              <NavDropdown.Item href={externalUrls.academy}>
                 Academy
               </NavDropdown.Item>
-              <NavDropdown.Item href="https://docs.okp4.network/whitepaper/abstract">
+              <NavDropdown.Item href={externalUrls.whitepaper}>
                 Whitepaper
               </NavDropdown.Item>
-              <NavDropdown.Item href="https://blog.okp4.network/">
-                Blog
-              </NavDropdown.Item>
+              <NavDropdown.Item href={externalUrls.blog}>Blog</NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="BUILD" className="fw-bold">
-              <NavDropdown.Item href="https://docs.okp4.network/technical-documentation/overview">
-                DOCS
-              </NavDropdown.Item>
-              <NavDropdown.Item href="https://github.com/okp4/">
+              <NavDropdown.Item href={externalUrls.doc}>DOCS</NavDropdown.Item>
+              <NavDropdown.Item href={externalUrls.github}>
                 GITHUB
               </NavDropdown.Item>
-              <NavDropdown.Item href="https://explore.okp4.network/OKP4%20Drunemeton%20testnet">
+              <NavDropdown.Item href={externalUrls.testnet}>
                 TESTNET
               </NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link
-              href="https://discord.com/invite/okp4"
-              className="fw-bold"
-            >
+            <Nav.Link href={externalUrls.discord} className="fw-bold">
               D
             </Nav.Link>
-            <Nav.Link href="https://github.com/okp4/" className="fw-bold">
+            <Nav.Link href={externalUrls.github} className="fw-bold">
               G
             </Nav.Link>
-            <Nav.Link href="https://blog.okp4.network/" className="fw-bold">
+            <Nav.Link href={externalUrls.blog} className="fw-bold">
               M
             </Nav.Link>
-            <Nav.Link href="https://t.me/okp4network" className="fw-bold">
+            <Nav.Link href={externalUrls.telegram} className="fw-bold">
               T
             </Nav.Link>
-            <Nav.Link
-              href="https://twitter.com/OKP4_Protocol"
-              className="fw-bold"
-            >
+            <Nav.Link href={externalUrls.x} className="fw-bold">
               X
             </Nav.Link>
           </Nav>
