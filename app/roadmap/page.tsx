6df6823996c4e01a1061ad8934af7e970ecf14ad
adriendillens <adriendillens@gmail.com>
2024-03-08 14:47:09 +0100

    metadata: title & description for home, about and roadmap pages

--- app/roadmap/page.tsx
+++ app/roadmap/page.tsx
@@ -1,7 +1,14 @@
+import type { Metadata } from "next";
 import IntroductionSection from "app/ui/roadmap/introduction/section";
 import LastYearsSection from "app/ui/roadmap/lastYears/section";
 import Q12024Section from "app/ui/roadmap/Q12024/section";
 import Q22024Section from "app/ui/roadmap/Q22024/section";
+
+export const metadata: Metadata = {
+  title: "Axone - Roadmap",
+  description:
+    "Axone roadmap is essential for understanding the project's trajectory and the anticipated significant developments",
+};
 
 export default function Roadmap() {
   return (
