66e2d5afbba2a2c5695780f6b58a02d4b825d855
ccamel <camel.christophe@gmail.com>
2024-03-27 15:54:36 +0100

    style: refine syntax & formatting

--- app/components/button/buttonsContainer.tsx
+++ app/components/button/buttonsContainer.tsx
@@ -5,7 +5,7 @@ const ButtonsContainer = ({
   rightButton,
 }: ButtonsContainerProps) => (
   <div className="grid w-full grid-cols-1 gap-5 sm:grid-cols-2">
-    <div className="flex justify-end btn-orange ">{leftButton}</div>
+    <div className="flex justify-end btn-orange">{leftButton}</div>
     <div className="flex justify-start btn-white">{rightButton}</div>
   </div>
 );
